b701cffeddd03b649a082b8761188065fe2bcca3
LouBdt <81627775+LouBdt@users.noreply.github.com>
2021-04-21 13:31:45 +0200

    Add files via upload

--- TestRT6.ino
+++ TestRT6.ino
@@ -23,33 +23,6 @@
   -VS1053 library by baldram (https://github.com/baldram/ESP_VS1053_Library)
   -ESP8266Wifi/WiFi
 
-  To run this example define the platformio.ini as below.
-
-  [env:nodemcuv2]
-  platform = espressif8266
-  board = nodemcuv2
-  framework = arduino
-  build_flags = -D PIO_FRAMEWORK_ARDUINO_LWIP2_HIGHER_BANDWIDTH
-  lib_deps =
-    ESP_VS1053_Library
-
-  [env:esp32dev]
-  platform = espressif32
-  board = esp32dev
-  framework = arduino
-  lib_deps =
-    ESP_VS1053_Library
-
-  Instructions:
-  -Build the hardware
-    (please find an additional description and Fritzing's schematic here:
-     https://github.com/vincegellar/Simple-Radio-Node#wiring)
-  -Set the station in this file
-  -Upload the program
-
-  IDE Settings (Tools):
-  -IwIP Variant: v1.4 Higher Bandwidth
-  -CPU Frequency: 160Hz
 */
 
 #include <VS1053.h>
@@ -71,7 +44,7 @@ VS1053 player(VS1053_CS, VS1053_DCS, VS1053_DREQ);
 WiFiClient client;
 
 // WiFi settings example, substitute your own
-const char *ssid = "YOUR SSDI";
+const char *ssid = "YOUR SSID";
 const char *password = "YOUR PASSWORD";
 
 //  https://listen.radioking.com/radio/372772/stream/422929
